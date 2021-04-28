280378c96a03d773ad9ec30e9e19682867baa865
Dimitris Kapanidis <d.kapanidis@gmail.com>
2021-04-28 12:37:37 +0200

    update priceUpdate popup

--- components/PriceUpdate.tsx
+++ components/PriceUpdate.tsx
@@ -11,13 +11,15 @@ export default function PriceUpdate() {
   return (
     <>
       {open && (
-        <div className="px-5 py-3 bg-black flex">
+        <div className="px-5 py-2 md:py-3 bg-black flex text-xs md:text-sm ">
+          <div className="flex-grow" />
           <Link href="/#pricing">
-            <a className="text-white text-center flex-grow">
+            <a className="text-white text-center">
               We're updating our pricing, with limited time 50% discount until 8
               of May.
             </a>
           </Link>
+          <div className="flex-grow"/>
           <span onClick={close} className="cursor-pointer text-white">
             x
           </span>
